094b799c022bc7d98e782f57c03fa8c7bbcaf362
NewAlexey <alexey.krupenia@gmail.com>
2024-10-03 11:54:32 +0300

    feat: implement tasks from "hash" module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+
+const fileDestination = "src/hash/files/fileToCalculateHashFor.txt";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash("sha256");
+    const stream = createReadStream(fileDestination);
+    stream.on("error", (error) => {
+        throw error;
+    });
+    stream.on("data", (chunk) => hash.update(chunk));
+    stream.on("end", () => console.log(hash.digest("hex")));
 };
 
 await calculateHash();
