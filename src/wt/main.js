dcb319466bde2ce2b49d7a331109dcb0bde6832e
NewAlexey <alexey.krupenia@gmail.com>
2024-10-03 15:43:58 +0300

    feat: implement tasks from "wt" module

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
+function getCpuCoresCount() {
+    return cpus().length;
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const coreCounts = getCpuCoresCount();
+    
+    const resultList = [];
+    
+    for (let i = 0; i < coreCounts; i++) {
+        const incrementalNumber = 35 + i;
+        
+        const worker = new Worker("./src/wt/worker.js", { workerData: incrementalNumber });
+        
+        worker.on("message", (data) => {
+            resultList[i] = { data, status: "resolved" };
+            
+            if (resultList.length === coreCounts) {
+                console.log(resultList);
+            }
+        })
+        
+        worker.on("error", () => {
+            resultList[i] = { data: null, status: "error" };
+            
+            if (resultList.length === coreCounts) {
+                console.log(resultList);
+            }
+        })
+    }
+    
 };
 
 await performCalculations();
